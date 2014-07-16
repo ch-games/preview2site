d2ca49620777228247eff2c9c825f34c3ebdcd8f
Mindaugas <Mindaugas@Mindaugas-PC>
2014-07-16 15:17:51 +0300

    test

--- sites/all/themes/bartik/templates/page.tpl.php
+++ sites/all/themes/bartik/templates/page.tpl.php
@@ -91,7 +91,7 @@
 
     <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
       </a>
     <?php endif; ?>
 
